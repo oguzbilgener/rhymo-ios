7ec37fd20656ceb001d5c647a1067ddbb4a58abe
Oğuz Bilgener <oguzbilgener@gmail.com>
2015-01-17 01:58:03 +0200

    empty song cell. gnight

---
+++ Rhymo-iOS/View/SongCell.swift
@@ -0,0 +1,13 @@
+//
+//  SongCell.swift
+//  Rhymo-iOS
+//
+//  Created by Oguz Bilgener on 17/01/15.
+//  Copyright (c) 2015 Oguz Bilgener. All rights reserved.
+//
+
+import UIKit
+
+class SongCell: UITableViewCell {
+  
+}
